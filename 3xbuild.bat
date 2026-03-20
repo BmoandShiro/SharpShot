3ef7e2fa0be8a49cd31414d43918750be9af9b21
BMO <Bmoandshiro@protonmail.com>
2026-03-20 14:54:55 -0400

    v1.2.8.5

--- 3xbuild.bat
+++ 3xbuild.bat
@@ -40,7 +40,7 @@ echo.
 REM --- Step 3: NSIS ---
 echo [3/3] Building NSIS installer...
 set "NSIS_SCRIPT=Installer\SharpShot.nsi"
-set "PORTABLE_DIR=SharpShot-Release-v1.2.8.4"
+set "PORTABLE_DIR=SharpShot-Release-v1.2.8.5"
 
 if not exist "%NSIS_SCRIPT%" (
   echo WARN: NSIS script not found. Skipping NSIS.
