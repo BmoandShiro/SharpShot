b155e14e5782112509bc84e46366a7caa0090e67
BMO <Bmoandshiro@protonmail.com>
2026-08-03 18:45:49 -0400

    WIP

--- SharpShot-Release-v1.2.9.7/Run OBS Studio.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ========================================
-echo    OBS Studio Launcher
-echo ========================================
-echo.
-echo Starting OBS Studio...
-echo.
-start "" "OBS-Studio\bin\64bit\obs64.exe"
--- SharpShot-Release-v1.2.9.8/Run SharpShot.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ========================================
-echo    SharpShot v1.2.9.8 - Release Package
-echo ========================================
-echo.
-echo Starting SharpShot...
-echo.
-start "" "SharpShot.exe"
